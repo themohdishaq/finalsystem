65f44e24417b5c1a9d1dc58c3ced9de5e8e033af
themohdishaq <ishaq10866@gmail.com>
2024-07-08 17:59:09 +0500

    all set

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -8,8 +8,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <GlobeAltIcon className="h-12 w-12 fs rotate-[15deg]" />
+      <p className="text-[42px]">Logixsy</p>
     </div>
   );
 }
